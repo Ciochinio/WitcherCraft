cbf9c9e9a499096367ed2e250cd508d258757136
Ciochinio <ciochu.michal@gmail.com>
2021-01-09 05:01:11 +0100

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,8 +537,9 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://68.183.121.7/distribution.json'
+        const distroURL = 'https://witchercraft.xyz/server/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
+        //http://68.183.121.7/distribution.json
         const opts = {
             url: distroURL,
             timeout: 2500

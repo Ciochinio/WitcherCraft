61367883bee76ea08b91d5bdb3493087475a59f8
Ciochinio <ciochu.michal@gmail.com>
2021-01-10 20:01:01 +0100

    Update landing.js

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -686,9 +686,9 @@ function dlAsync(login = true){
                 const gameStateChange = function(data){
                     data = data.trim()
                     if(SERVER_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Exploring the Realm!')
+                        DiscordWrapper.updateDetails('POPOGA1!')
                     } else if(GAME_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Sailing to Westeros!')
+                        DiscordWrapper.updateDetails('POPOGA2!')
                     }
                 }
 
